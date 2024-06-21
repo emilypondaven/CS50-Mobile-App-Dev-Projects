8f8b6fd2552d112cac6975c78b3315a345767820
emilypondaven <emily.pondaven@gmail.com>
2024-06-21 23:12:22 +0100

    edits to script.js

--- project_0/script.js
+++ project_0/script.js
@@ -1,3 +1,5 @@
+// Links any element I create in js with the associated CSS class name
+// Affects how it looks
 const classNames = {
   TODO_ITEM: 'todo-container',
   TODO_CHECKBOX: 'todo-checkbox',
@@ -5,10 +7,49 @@ const classNames = {
   TODO_DELETE: 'todo-delete',
 }
 
+// A specific section of the website (HTML section)
 const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
+
 function newTodo() {
-  alert('New TODO button clicked!')
+  // Ask user to input the task
+  let newTask = prompt("Please enter your next task below");
+
+  // Empty input exception
+  if (newTask == "") {
+    alert("No task inputted")
+  } else {
+    addTodo(newTask)
+  }
 }
+
+function addText(text) {
+
+}
+
+function deleteToDo(text) {
+
+}
+
+function addCheckbox(text) {
+
+}
+
+function addTodo(text) {
+  
+}
+
+
+// NOTES:
+// document.createElement(**tagname**) -> creates new HTML element
+// document.getElementById() -> returns the element that has the id attribute
+// element.innerHTML -> used to access current HTML
+
+// Button click
+/* let element = document.getElementById('addItem');
+element.onclick = function() { 
+  let newElement = document.createElement('li');
+  document.getElementById('list').appendChild(newElement);
+}; */
