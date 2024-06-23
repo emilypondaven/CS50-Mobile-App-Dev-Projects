3187fcafbd1187914517f186f483e49878681e8b
emilypondaven <emily.pondaven@gmail.com>
2024-06-24 00:58:17 +0100

    working to do list

--- project_0/script.js
+++ project_0/script.js
@@ -1,3 +1,7 @@
+// Start local server
+// python -m http.server
+// http://localhost:8000
+
 // Links any element I create in js with the associated CSS class name
 // Affects how it looks
 const classNames = {
@@ -12,6 +16,12 @@ const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
+fetch('data.json')
+    .then((response) => response.json())
+    .then((data) => {
+      itemCountSpan.innerHTML = data['item count'];
+      uncheckedCountSpan.innerHTML = data['unchecked count'];
+    });
 
 function newTodo() {
   // Ask user to input the task
@@ -19,26 +29,68 @@ function newTodo() {
 
   // Empty input exception
   if (newTask == "") {
-    alert("No task inputted")
+    alert("No task inputted");
   } else {
-    addTodo(newTask)
+    addTodo(newTask);
   }
 }
 
 function addText(text) {
-
+  var span = document.createElement("span")
+  span.setAttribute("class", classNames["TODO_TEXT"]);
+  span.innerText = text;
+  return span;
 }
 
-function deleteToDo(text) {
+function addCheckbox(id) {
+  var input = document.createElement("input");
+  input.setAttribute("type", "checkbox");
+  input.setAttribute("class", classNames['TODO_CHECKBOX']);
+  input.setAttribute("id", "checkbox" + id);
+  console.log("checkbox" + id);
+  input.addEventListener("change", function() {
+    if (this.checked) {
+      uncheckedCountSpan.innerHTML--;
+    } else {
+      uncheckedCountSpan.innerHTML++;
+    }
+  });
 
+  return input;
 }
 
-function addCheckbox(text) {
+function addDeleteButton(id) {
+  var button = document.createElement("button");
+  button.setAttribute("class", classNames['TODO_DELETE']);
+  button.innerHTML = "delete";
+  button.addEventListener("click", function() {
+    itemCountSpan.innerHTML--;
+    // Get whether checkbox is checked
+    var checked = document.getElementById("checkbox" + id).checked
+    if (!checked) {
+      uncheckedCountSpan.innerHTML--;
+    }
+
+    list.removeChild(document.getElementById(id));
+  });
+
+  return button;
 
 }
 
 function addTodo(text) {
-  
+  var li = document.createElement("li")
+  li.setAttribute("class", classNames["TODO_ITEM"]);
+  li.setAttribute("id", itemCountSpan.innerHTML);
+
+  li.appendChild(addCheckbox(li.id));
+  li.appendChild(addText(text));
+  li.append(addDeleteButton(li.id));
+
+  list.appendChild(li);
+ 
+  itemCountSpan.innerHTML++;
+  uncheckedCountSpan.innerHTML++;
 }
 
 
